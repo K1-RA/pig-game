ce0c312930fabbcbe91a2f33446154600dac7baf
K1-RA <indrajeetgodse@gmail.com>
2022-05-10 00:49:55 +0200

    Update
    Finished all JS related work

--- script.js
+++ script.js
@@ -1,1 +1,98 @@
-'use strict';
+"use strict";
+
+/// Selecting elements
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.querySelector("#score--0");
+/// instead of using the query selector, we can use 'get element by ID' if we are targetting a specific ID. This method is faster as it directly points to the element
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+let scores, currentScore, activePlayer, playing;
+
+/// Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player1El.classList.remove("player--active");
+  player0El.classList.add("player--active");
+};
+
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
+
+/// Implementing rolling dice functionaliy
+
+btnRoll.addEventListener("click", function () {
+  if (playing) {
+    // 1. Generating a random number between 1-6
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // 2. Display dice
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
+    // 3. Check for rolled 1
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+/// Implementing holding score functionaliy
+
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    // 1. add current score to score of active player
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    currentScore = 0;
+    // 2. Check score is >= 100
+
+    if (scores[activePlayer] >= 100) {
+      // End game
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      // If not, switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+/// Resetting the game
+
+btnNew.addEventListener("click", init);
